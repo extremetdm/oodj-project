b8548d6ec6cfcdd3fa3210658e3e0b4b7c9e7a18
JosephIsNoob <unidentifiedperson117@gmail.com>
2026-02-13 21:25:34 +0800

    update random seed

--- src/main/java/oodj_project/features/dashboard/user_management/PasswordGenerator.java
+++ src/main/java/oodj_project/features/dashboard/user_management/PasswordGenerator.java
@@ -1,12 +1,13 @@
 package oodj_project.features.dashboard.user_management;
 
+import java.util.Date;
 import java.util.Random;
 
 public class PasswordGenerator {
     public static String generate(int length) {
         String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder sb = new StringBuilder();
-        Random random = new Random();
+        Random random = new Random(new Date().getTime());
         for (int i = 0; i < length; i++) {
             sb.append(chars.charAt(random.nextInt(chars.length())));
         }
